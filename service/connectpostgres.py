c5e434e26a9c19448faa54cfc95402404aa32b47
sarawut.ki <sarawut.ki@mail.com>
2023-09-21 11:56:23 +0700

    Update Connection

--- service/connectpostgres.py
+++ service/connectpostgres.py
@@ -5,20 +5,23 @@ import psycopg2
 def connectdb():
     # local
 
+    # try:
+    #     connection = psycopg2.connect(
+    #         host='localhost',
+    #         user='postgres',
+    #         password='1234',
+    #         database='sampledb',
+    #         port=5432
+    #     )
+    #     return connection
+    # except Exception as e:
+    #     print(e)
     try:
-        # connection = psycopg2.connect(
-        #     host='localhost',
-        #     user='root',
-        #     password='1234',
-        #     db='postgres',
-        #     charset='utf8mb4',
-        #     port=3306,
-        #     cursorclass=psycopg2.cursors.DictCursor)
         connection = psycopg2.connect(
-            host='localhost',
-            user='postgres',
-            password='1234',
-            database='sampledb',
+            host='dpg-ck5snnldrqvc73dv2kh0-a',
+            user='root',
+            password='UogxOvefAybDosSjMTIhOzF67YF2dexX',
+            database='sampledb_12qn',
             port=5432
         )
         return connection
